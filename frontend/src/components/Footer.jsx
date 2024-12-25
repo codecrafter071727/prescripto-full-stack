c1f22f0913394a3d4686750873a558d4c46cfd21
Anshul Bhathija <anshulbhathija1727@gmail.com>
2024-12-25 23:23:13 +0530

    second commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>"Prescripto – Simplifying healthcare, empowering lives. We are dedicated to connecting patients and healthcare providers through innovation and technology, ensuring convenient access to care and seamless management of health records. Trust us to support you at every step of your healthcare journey.</p>
         </div>
 
         <div>
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+91 8815918808</li>
+            <li>anshulbhathija966@gamil.com</li>
           </ul>
         </div>
 
